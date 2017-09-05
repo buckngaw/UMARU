2d6fbd2ad884de8e6a1f131c2714d312d19d1d89
buckngaw <aon_natjaya@hotmail.com>
2017-09-05 11:57:09 +0700

    StateMachine
    -Ready Board
    -Camera follow Indicator
    -Can move indicator around tiles

---
+++ TacticMath/Assets/Scripts/View Model Component/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    //can load our LevelData and create a game board level at run-time
+    [SerializeField] GameObject tilePrefab;
+    public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
+    public void Load(LevelData data)
+    {
+        for (int i = 0; i < data.tiles.Count; ++i)
+        {
+            GameObject instance = Instantiate(tilePrefab) as GameObject;
+            Tile t = instance.GetComponent<Tile>();
+            t.Load(data.tiles[i]);
+            tiles.Add(t.pos, t);
+        }
+    }
+}
